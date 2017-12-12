89580cbbf91980fae60ebaeaa320f0c2fe679d35
FredrickI <fredrick.ifebuzo@gmail.com>
2017-12-11 23:13:58 -0500

    Update wordvalue-template.py

--- 01/wordvalue-template.py
+++ 01/wordvalue-template.py
@@ -1,18 +1,29 @@
 from data import DICTIONARY, LETTER_SCORES
 
 def load_words():
-    """Load dictionary into a list and return list"""
-    pass
+    dict = open(DICTIONARY, 'r+')
+    lst = []
+    for i in dict:
+        lst.append(i)
+    lst = [word.rstrip() for word in lst]
+    return lst
+    
 
-def calc_word_value():
+def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    pass
+    return sum([LETTER_SCORES[letter] for letter.upper() in word]
 
-def max_word_value():
+def max_word_value(words):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    pass
+    counter = 0
+	for word in words:
+		if calc_word_value(word) > 0:
+			counter = calc_word_value(word)
+		else:
+			continue
+	return counter
 
 if __name__ == "__main__":
     pass # run unittests to validate
