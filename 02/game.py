7b841ea8ca62905314e24dbf7837c9713305c26c
FredrickI <fredrick.ifebuzo@gmail.com>
2017-12-18 18:21:51 -0500

    Update and rename game-help.py to game.py

--- 02/game.py
+++ 02/game.py
@@ -1,7 +1,7 @@
 #!python3
-# Code Challenge 02 - Word Values Part II - a simple game
-# http://pybit.es/codechallenge02.html
+#game.py
 
+    
 import itertools
 import random
 
@@ -12,19 +12,41 @@ NUM_LETTERS = 7
 
 def draw_letters():
     """Pick NUM_LETTERS letters randomly. Hint: use stdlib random"""
-    pass
+    letters = random.choices(POUCH , k = NUM_LETTERS)
+    return letters
+    
 
 
 def input_word(draw):
     """Ask player for a word and validate against draw.
     Use _validation(word, draw) helper."""
-    pass 
+    print("Your letters are %s " % (draw))
+    word = input("Input a word from your letters")
+    valid = _validation(word,draw)
+    if valid == True:
+        return word
+    else:
+        return "Please enter a valid word and try again"
+    
 
 
 
 def _validation(word, draw):
     """Validations: 1) only use letters of draw, 2) valid dictionary word"""
-    pass
+    switch = True
+    while switch:
+        for letter in word:
+            if letter in draw:
+                continue
+            else:
+                return "You don't have that letter"
+                
+        if word in DICTIONARY:
+            print("%s is a valid word" % (word))
+            return True
+        else:
+            print("This is not a valid word. Try again")
+            return False
 
 
 # From challenge 01:
@@ -40,13 +62,21 @@ def calc_word_value(word):
 def get_possible_dict_words(draw):
     """Get all possible words from draw which are valid dictionary words.
     Use the _get_permutations_draw helper and DICTIONARY constant"""
-    pass
+    combos =_get_permutations_draw(draw)
+    lst = []
+    for i in combos:
+        if i in DICTIONARY:
+                  lst.append(i)
+    return lst
+    
 
 
 def _get_permutations_draw(draw):
     """Helper for get_possible_dict_words to get all permutations of draw letters.
     Hint: use itertools.permutations"""
-    pass
+    perm = itertools.permutations(draw)
+    return perm
+
 
 
 # From challenge 01:
@@ -77,3 +107,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
