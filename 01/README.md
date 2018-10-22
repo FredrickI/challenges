ed4b45b4910f7b524c8be55291ad3004eeb9e80e
FredrickI <fredrick.ifebuzo@gmail.com>
2018-10-21 22:49:38 -0400

    Add files via upload

--- 01/README.md
+++ 01/README.md
@@ -1,5 +1,20 @@
-## Code Challenge 01 - Word Values Part I
+# Word Values Challenge Part I
+
+Word Values is a scrabble game that takes a dictionary word and calculates the value. 
+
+You can also take a list of words and find which one has the highest value. If no word is given it defaults to calculate the 
+
+the max value from the dictionary.
+
+Use the test_wordvalue.py to verify it works.  
+
+
+
+
+### Acknowledgement 
+
+Bob Belderbos for the load_words function. 
+
 
 Instructions [here](http://pybit.es/codechallenge01.html).
 
-Previous challenges and About [here](http://pybit.es/pages/challenges.html).
--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -1,29 +1,32 @@
 from data import DICTIONARY, LETTER_SCORES
 
+
 def load_words():
     with open(DICTIONARY) as d:
         return [word.rstrip() for word in d.read().split()]
-    
+
 
 def calc_word_value(word):
-    """Calculate the value of the word entered into function
+    """Calculate the value of the word entered   into function
     using imported constant mapping LETTER_SCORES"""
     count = 0
     for letter in word:
-        count += LETTER_SCORES[letter.upper()]
+        count += LETTER_SCORES.get(letter.upper(),0)
     return count
 
-def max_word_value(words):
+
+
+def max_word_value(words=load_words()):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
     counter = 0
-	for word in words:
-		if calc_word_value(word) > counter:
-			counter = calc_word_value(word)
-	       		max_word = word
-		else:
-			continue
-	return "%s is the word with a max value of %s" % (max_word, counter)
 
-if __name__ == "__main__":
-    pass # run unittests to validate
+    for word in words:
+        if calc_word_value(word) > counter:
+            counter = calc_word_value(word)
+            max_word = word
+
+
+    print("%s is the word with a max value of %s" % (max_word, counter))
+    return max_word
+
